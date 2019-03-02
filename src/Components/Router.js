9646a7d6d07daf04aef361fb64d9c48b7eb33b3c
Anshul Mittal <anshulmittal0409@gmail.com>
2019-03-02 20:27:57 +0530

    Add User Home screen
    Signed-off-by: Anshul Mittal <anshulmittal0409@gmail.com>

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -2,10 +2,12 @@ import { createStackNavigator, createAppContainer } from 'react-navigation';
 
 import EntryScreen from './EntryScreen';
 import HomeScreen from './HomeScreen';
+import UserHome from './User/Home';
 
 const AppNavigator = createStackNavigator({
   EntryScreen: { screen: EntryScreen },
   HomeScreen: { screen: HomeScreen },
+  UserHome: { screen: UserHome },
 });
 
 export default createAppContainer(AppNavigator);

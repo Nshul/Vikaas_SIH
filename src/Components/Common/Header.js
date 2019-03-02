d5d6f9e35330861d96649486963d68b4c6ffe39e
Anshul Mittal <anshulmittal0409@gmail.com>
2019-03-02 20:28:52 +0530

    Add header for each screen
    Signed-off-by: Anshul Mittal <anshulmittal0409@gmail.com>

---
+++ src/Components/Common/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Header } from 'react-native-elements';
+
+import HamburgerMenu from './HamburgerMenu';
+
+const MyHeader = props => {
+  return (
+    <Header
+      leftComponent={<HamburgerMenu navigation={props.navigation} />}
+      centerComponent={{
+        text: props.title,
+        style: { color: '#fff', fontWeight: 'bold', fontSize: 30 },
+      }}
+      statusBarProps={{ barStyle: 'light-content' }}
+      containerStyle={{
+        backgroundColor: '#009688',
+        borderBottomLeftRadius: 40,
+        borderBottomRightRadius: 40,
+        marginBottom: 4,
+      }}
+    />
+  );
+};
+
+export default MyHeader;

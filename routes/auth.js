17564b12c7359ff8baa63d034536018a947121a0
Anshul Kumar Singh <anshul98ks123@gmail.com>
2019-03-03 10:16:23 +0530

    hardcode auth
    Signed-off-by: Anshul Kumar Singh <anshul98ks123@gmail.com>

--- routes/auth.js
+++ routes/auth.js
@@ -7,85 +7,85 @@ const express = require('express'),
 router.post('/requestotp', (req, response) => {
 	console.log(req.body);
 
-	authy.phones().verification_start(req.body.phone, '91', 'sms', function(err, res) {
-		if (err) {
-			console.log(err);
-			return response.send(err);
-		}
+	// authy.phones().verification_start(req.body.phone, '91', 'sms', function(err, res) {
+	// 	if (err) {
+	// 		console.log(err);
+	// 		return response.send(err);
+	// 	}
 
-		// response.render('otp');
-		return response.send('success');
-	});
+	// 	// response.render('otp');
+	// 	return response.send('success');
+	// });
 
-	// return response.send('success');
+	return response.send('success');
 });
 
 router.post('/user/verifyotp', (req, response) => {
-	authy.phones().verification_check(req.body.phone, '91', req.body.otp, function(err, res) {
-		if (err) {
-			// invalid token
-			console.log(err);
-			return response.send(err);
-		}
-
-		User.find({
-			phone: req.body.phone
-		})
-			.then((User) => {
-				return response.send(User);
-			})
-			.catch((err) => {
-				console.log(err);
-
-				return response.send(err);
-			});
-	});
-
-	// User.find({
-	// 	phone: req.body.phone
-	// })
-	// 	.then((User) => {
-	// 		return response.send(User);
-	// 	})
-	// 	.catch((err) => {
+	// authy.phones().verification_check(req.body.phone, '91', req.body.otp, function(err, res) {
+	// 	if (err) {
+	// 		// invalid token
 	// 		console.log(err);
-
 	// 		return response.send(err);
-	// 	});
+	// 	}
+
+	// 	User.find({
+	// 		phone: req.body.phone
+	// 	})
+	// 		.then((User) => {
+	// 			return response.send(User);
+	// 		})
+	// 		.catch((err) => {
+	// 			console.log(err);
+
+	// 			return response.send(err);
+	// 		});
+	// });
+
+	User.find({
+		phone: req.body.phone
+	})
+		.then((User) => {
+			return response.send(User);
+		})
+		.catch((err) => {
+			console.log(err);
+
+			return response.send(err);
+		});
 });
 
 router.post('/mp/verifyotp', (req, response) => {
-	authy.phones().verification_check(req.body.phone, '91', req.body.otp, function(err, res) {
-		if (err) {
-			// invalid token
-			response.send(err);
-			err;
-		}
+	// authy.phones().verification_check(req.body.phone, '91', req.body.otp, function(err, res) {
+	// 	if (err) {
+	// 		// invalid token
+	// 		response.send(err);
+	// 		err;
+	// 	}
 
-		Mp.find({
-			phone: req.body.phone
-		})
-			.then((mp) => {
-				if (!!!mp) {
-					return response.send(err);
-				}
-
-				return response.send(mp);
-			})
-			.catch((err) => {
-				return response.send(err);
-			});
-	});f
-
-	// User.find({
-	// 	phone: req.body.phone
-	// })
-	// 	.then((User) => {
-	// 		return response.send(User);
+	// 	Mp.find({
+	// 		phone: req.body.phone
 	// 	})
-	// 	.catch((err) => {
-	// 		return response.send(err);
-	// 	});
+	// 		.then((mp) => {
+	// 			if (!!!mp) {
+	// 				return response.send(err);
+	// 			}
+
+	// 			return response.send(mp);
+	// 		})
+	// 		.catch((err) => {
+	// 			return response.send(err);
+	// 		});
+	// });
+
+	User.find({
+		phone: req.body.phone
+	})
+		.then((User) => {
+			return response.send(User);
+		})
+		.catch((err) => {
+			return response.send(err);
+		});
 });
 
 module.exports = router;

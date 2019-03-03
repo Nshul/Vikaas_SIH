b0cad3886ffa63b05bc42c9667a22bebf8ad266d
Anshul Kumar Singh <anshul98ks123@gmail.com>
2019-03-03 10:28:51 +0530

    heroku trial
    Signed-off-by: Anshul Kumar Singh <anshul98ks123@gmail.com>

--- server.js
+++ server.js
@@ -24,6 +24,6 @@ app.use('/', userRoutes);
 app.use('/', commentRoutes);
 app.use('/', mpRoutes);
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
 	console.log('Server has started');
 });

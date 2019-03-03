7ae00260cae069e47ddce8830eef2007759a2dc9
Anshul Mittal <anshulmittal0409@gmail.com>
2019-03-03 12:49:28 +0530

    Add python shell
    Signed-off-by: Anshul Mittal <anshulmittal0409@gmail.com>

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express'),
-	app = express(),
-	bodyParser = require('body-parser'),
-	mongoose = require('mongoose');
+  app = express(),
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose');
 
 const indexRoutes = require('./routes/index'),
-	authRoutes = require('./routes/auth'),
-	userRoutes = require('./routes/user'),
-	commentRoutes = require('./routes/comment'),
-	mpRoutes = require('./routes/mp'),
-	complaintRoutes = require('./routes/complaint');
+  authRoutes = require('./routes/auth'),
+  userRoutes = require('./routes/user'),
+  commentRoutes = require('./routes/comment'),
+  mpRoutes = require('./routes/mp'),
+  complaintRoutes = require('./routes/complaint'),
+  pythonScripts = require('./routes/pythonScripts');
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://vikaas:sih2019@ds029106.mlab.com:29106/sih');
@@ -23,7 +24,8 @@ app.use('/', complaintRoutes);
 app.use('/', userRoutes);
 app.use('/', commentRoutes);
 app.use('/', mpRoutes);
+app.use('/', pythonScripts);
 
 app.listen(8000, () => {
-	console.log('Server has started');
+  console.log('Server has started');
 });
